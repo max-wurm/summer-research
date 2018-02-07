8d8d8374c46e3f13e4e224d69e3d6b4ec9b7387a
Max Wurm <34562453+max-wurm@users.noreply.github.com>
2018-02-07 12:18:06 +1030

    Update plot_comparison.R
    Reworked plotting so that bottleneck is represented by NANC rather than RESIZE. Also changed the way the exponential model was plotted to include historical event which changes growth rate to 0.

--- max code/plot_comparison.R
+++ max code/plot_comparison.R
@@ -1,7 +1,8 @@
 # plotting constant, exponential and bottleneck functions - created by Max Wurm: last edited 25/01/18
 # Description: this code produces a plot showing a constant, exponential and bottleneck function (as functions of the specified values).
 #   The specification should be in a .txt file, containing the names of the parameters on the first line, then the values on the second line.
-#   The order should be N_0 min, N_0 max, growth rate, bottleneck min time, bottleneck max time, bottleneck resize factor
+#   The order should be N_0 min, N_0 max, growth rate min, growth rate max, NANC min, NANC max, bottleneck min time, bottleneck max time, bottleneck resize factor
+#   (NANC is the population size that you want after the exponential model becomes constant)
 
 # user set parameters
 T = 500 # number of generations to plot for
@@ -11,14 +12,20 @@ spec <- read.table("specification.txt", header = TRUE)
 thenames <- names(spec)
 
 # get plotting data into a dataframe
-constant_min <- rep(spec[[1]],T+1)
-constant_max <- rep(spec[[2]],T+1)
-exponential_min = spec[[1]]*exp(spec[[3]]*0:T)
-exponential_max = spec[[2]]*exp(spec[[3]]*0:T)
-bottleneck_min = c(rep(spec[[1]],spec[[5]]),rep(spec[[6]]*spec[[1]],T+1 - spec[[5]]))
-bottleneck_max = c(rep(spec[[2]],spec[[4]]),rep(spec[[6]]*spec[[2]],T+1 - spec[[4]]))
-botmean <- floor((spec[[4]] + spec[[5]])/2)
+constant_min <- rep(spec$NPOP_min,T+1)
+constant_max <- rep(spec$NPOP_max,T+1)
+exponential_min = spec$NPOP_min*exp(spec$GROWTH_max*0:T) # this is before replacing the later values with a constant model
+exponential_max = spec$NPOP_max*exp(spec$GROWTH_min*0:T) # ""
+bottleneck_min = c(rep(spec$NPOP_min - spec$BOTDIFF, spec$TBOT_max), rep(spec$NPOP_min + spec$BOTDIFF, T+1 - spec$TBOT_max))
+bottleneck_max = c(rep(spec$NPOP_max - spec$BOTDIFF, spec$TBOT_min), rep(spec$NPOP_max + spec$BOTDIFF, T+1 - spec$TBOT_min))
+botmean <- floor((spec$TBOT_min + spec$TBOT_max)/2)
 bottleneck_mean = c(rep((bottleneck_min[1]+bottleneck_max[1])/2, botmean), rep((bottleneck_min[T]+bottleneck_max[T])/2,T+1 - botmean))
+
+# replace exponential things
+exptime_min = (1/spec$GROWTH_max) * log(spec$NANC_min/spec$NPOP_min)
+exptime_max = (1/spec$GROWTH_min) * log(spec$NANC_max/spec$NPOP_max)
+exponential_min[exptime_min:(T+1)] = rep(spec$NANC_min,T+2-exptime_min)
+exponential_max[exptime_max:(T+1)] = rep(spec$NANC_max,T+2-exptime_max)
 
 alldata <- data.frame(Time = 0:T,
                       conmin = constant_min,
@@ -33,14 +40,17 @@ alldata <- data.frame(Time = 0:T,
 
 # make plot
 allmodels <- ggplot(alldata, aes(x = Time)) +
-  geom_ribbon(aes(ymin = conmin, ymax = conmax, fill = "constant"), alpha = 0.3, fill = "green", color = "green", linetype = "dashed") + 
-  geom_ribbon(aes(ymin = expmin, ymax = expmax, fill = "exponential"), alpha = 0.3, fill = "blue", color = "blue", linetype = "dashed") +
-  geom_ribbon(aes(ymin = botmin, ymax = botmax, fill = "bottleneck"), alpha = 0.3, fill = "red", color = "red", linetype = "dashed") +
-  geom_line(aes(y = conmean), color = "green", alpha = 1, linetype = "solid") +
+  geom_ribbon(aes(ymin = conmin, ymax = conmax, colour = "constant"), alpha = 0.3, fill = "green", color = "green", linetype = "dashed") + 
+  geom_ribbon(aes(ymin = expmin, ymax = expmax, colour = "exponential"), alpha = 0.3, fill = "blue", color = "blue", linetype = "dashed") +
+  geom_ribbon(aes(ymin = botmin, ymax = botmax, colour = "bottleneck"), alpha = 0.3, fill = "red", color = "red", linetype = "dashed") +
+  geom_line(aes(y = conmean), color = "green", alpha = 1, linetype = "dashed") +
   geom_line(aes(y = expmean), color = "blue", alpha = 1, linetype = "dashed") +
-  geom_line(aes(y = botmean), color = "red", alpha = 1, linetype = "dashed")
+  geom_line(aes(y = botmean), color = "red", alpha = 1, linetype = "dashed") +
+  theme_classic() +
+  labs(y = "Estimated Population Size", title = "Model Comparison", x = "Time (generations before present)") +
+  scale_colour_manual(values = c("constant" = "green", "exponential" = "blue", "bottleneck" = "red"), labels = c("constant","exponential","bottleneck"))
 
-# allmodels # uncomment to display the plot when run
+allmodels # uncomment to display the plot when script is run
 
 # save the plot
-ggsave(filename = "allmodels.pdf", plot = allmodels, device = "pdf")
+ggsave(filename = "figures/allmodels.png", plot = allmodels, device = "png")
